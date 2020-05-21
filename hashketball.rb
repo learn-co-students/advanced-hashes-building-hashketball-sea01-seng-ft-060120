e4f54d54f584ba767c4f0c5b71df7d0e9e0f793d
Gabriel Castro <gabriel.castro1028@gmail.com>
2020-05-21 22:10:26 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,24 @@
-# Write your code here!
+require 'pry'
+
+def game_hash
+  
+  home:{
+    team_name: "Brooklyn Nets",
+    team_colors:["Black", "Purple"],
+    players:{
+      
+    }
+    
+  }
+  
+  away:{
+    team_name: "Charlotte Hornets",
+    team_colors: ["Turquoise", "Purple"],
+    players {
+      
+    }
+    
+  }
+  
+end
+  
